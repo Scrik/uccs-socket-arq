42eeba8fae608c79f6e25f2ea64d8dfc6ae03141
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-05 23:26:59 -0700

    trying to simulate dropped packets; can't even get a printout to work though...

--- part2_udp/udp_server.c
+++ part2_udp/udp_server.c
@@ -3,15 +3,21 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <time.h>
 
-#define SERVER_UDP_PORT      5000   
-#define MAXLEN      4096
+#define SERVER_UDP_PORT     5000
+#define MAXLEN      		4096
+#define DROP_RATE			50
 
 int main(int argc, char **argv)
 {
+	printf("FOOO!");
    int    sd, client_len, port, n;
    char    buf[MAXLEN];
    struct    sockaddr_in    server, client;
+
+   // Seed the random number generator
+   srand(time(NULL));
 
    switch(argc) {
    case 1:
@@ -24,6 +30,8 @@ int main(int argc, char **argv)
       fprintf(stderr, "Usage: %s [port]\n", argv[0]);
       exit(1);
    }
+
+   printf("Using port %d", port);
       
    /* Create a datagram socket */
    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
@@ -49,6 +57,26 @@ int main(int argc, char **argv)
       (struct sockaddr *)&client, &client_len)) < 0) {
             fprintf(stderr, "Can't receive datagram\n");
             exit(1);
+      }
+
+      // Modify the buffer to simulate dropped packets
+      int m = 0;	// Size of modified buffer
+      int i, j, r;	// i: index of original buffer
+      				// j: index of new buffer
+      				// r: random number deciding whether to drop packet
+      char droppedBuf[MAXLEN];
+      printf("HELLO WORLD!");
+      for(i = 0; i < n; i++) {
+      	r = 1 + (rand() % 100);	// range of 1-100
+      	// if( r > DROP_RATE ) {
+      	if( I%2 == 0 ) {
+      		// Don't drop the packet
+      		droppedBuf[j] = buf[i];
+      		j++;
+      	} else {
+      		// By not copying over the element, we "drop" it
+      		// DO NOTHING, intentionally
+      	}
       }
 
       if (sendto(sd, buf, n, 0, 
