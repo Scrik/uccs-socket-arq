ac4a9762cbdcd234a83594949f51de168b054c55
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-07 11:20:56 -0700

    Bug fix: forgot to initialize [j] the count of non-dropped pkts

--- part2_udp/udp_server.c
+++ part2_udp/udp_server.c
@@ -7,7 +7,7 @@
 
 #define SERVER_UDP_PORT     5000
 #define MAXLEN      		4096
-#define DROP_RATE			20
+#define DROP_RATE			10
 
 int main(int argc, char **argv)
 {
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
       
    /* Create a datagram socket */
    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-      fprintf(stderr, "END Can't create a socket\n");
+      fprintf(stderr, "END [FAILURE] Can't create a socket\n");
       exit(1);
    }
 
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
    server.sin_addr.s_addr = htonl(INADDR_ANY);
    if (bind(sd, (struct sockaddr *)&server, 
    sizeof(server)) == -1) {
-      fprintf(stderr, "Can't bind name to socket\n");
+      fprintf(stderr, "END [FAILURE] Can't bind name to socket\n");
       exit(1);
    }
 
@@ -54,8 +54,8 @@ int main(int argc, char **argv)
       client_len = sizeof(client);
       if ((n = recvfrom(sd, buf, MAXLEN, 0, 
       (struct sockaddr *)&client, &client_len)) < 0) {
-            fprintf(stderr, "END Can't receive datagram\n");
-            exit(1);
+            fprintf(stderr, "END [FAILURE] Can't receive datagram\n");
+            continue;
       }
 
 
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
       char droppedBuf[n];
       printf("   START iterating over packets! BufIn.len = %d\n", n);
       printf("      ");
-      for(i = 0; i < n; i++) {
+      for(i=0, j=0; i < n; i++) {
       	r = 1 + (rand() % 100);	// range of 1-100
          printf("(%2d)", i);
       	if( r > DROP_RATE ) {
@@ -89,10 +89,10 @@ int main(int argc, char **argv)
 
       if (sendto(sd, droppedBuf, j, 0, 
       (struct sockaddr *)&client, client_len) != n) {
-            fprintf(stderr, "END Can't send datagram\n");
+            fprintf(stderr, "END [FAILURE] Can't send datagram\n");
             continue;
       } else {
-         printf("END Datagram sent\n");
+         printf("END [SUCCESS] Datagram sent\n");
       }
    }
    close(sd);
