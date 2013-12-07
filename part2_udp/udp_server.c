e705ca75a1aaf572ea21d4eb82dac6956b6b4b2d
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-07 11:03:39 -0700

    error sending datagram, but server appears to be dropping packets from the buffer

--- part2_udp/udp_server.c
+++ part2_udp/udp_server.c
@@ -7,11 +7,10 @@
 
 #define SERVER_UDP_PORT     5000
 #define MAXLEN      		4096
-#define DROP_RATE			50
+#define DROP_RATE			20
 
 int main(int argc, char **argv)
 {
-	printf("FOOO!");
    int    sd, client_len, port, n;
    char    buf[MAXLEN];
    struct    sockaddr_in    server, client;
@@ -31,7 +30,7 @@ int main(int argc, char **argv)
       exit(1);
    }
 
-   printf("Using port %d", port);
+   printf("Using port %d\n", port);
       
    /* Create a datagram socket */
    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
@@ -59,30 +58,41 @@ int main(int argc, char **argv)
             exit(1);
       }
 
+
+      printf("START Sending datagram...\n");
+      printf("   CLIENT => client_len=%d, n=%d\n", client_len, n);
+
       // Modify the buffer to simulate dropped packets
       int m = 0;	// Size of modified buffer
       int i, j, r;	// i: index of original buffer
       				// j: index of new buffer
       				// r: random number deciding whether to drop packet
       char droppedBuf[MAXLEN];
-      printf("HELLO WORLD!");
+      printf("   START iterating over packets! BufIn.len = %d\n", n);
+      printf("      ");
       for(i = 0; i < n; i++) {
       	r = 1 + (rand() % 100);	// range of 1-100
-      	// if( r > DROP_RATE ) {
-      	if( I%2 == 0 ) {
+         printf("(%2d)", i);
+      	if( r > DROP_RATE ) {
+      	// if( i%2 == 0 ) {
+            printf(".");
       		// Don't drop the packet
       		droppedBuf[j] = buf[i];
       		j++;
       	} else {
+            printf("DROP\n      ");
       		// By not copying over the element, we "drop" it
       		// DO NOTHING, intentionally
       	}
       }
+      printf("\n   END iterating over packets! BufOut.len = %d\n", j);
 
-      if (sendto(sd, buf, n, 0, 
+      if (sendto(sd, droppedBuf, j, 0, 
       (struct sockaddr *)&client, client_len) != n) {
-            fprintf(stderr, "Can't send datagram\n");
+            fprintf(stderr, "END Can't send datagram\n");
             exit(1);
+      } else {
+         printf("END Datagram sent\n");
       }
    }
    close(sd);
