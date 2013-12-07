24f1246976a5d1d53dcb88d75af7f44cc853d998
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-07 11:39:53 -0700

    move the server send functionality to a function in prep for doing the same with the other strategies

--- part2_udp/udp_server.c
+++ part2_udp/udp_server.c
@@ -5,9 +5,9 @@
 #include <netinet/in.h>
 #include <time.h>
 
-#define SERVER_UDP_PORT     5000
-#define MAXLEN      		4096
-#define DROP_RATE			10
+#define SERVER_UDP_PORT    5000
+#define MAXLEN             4096
+#define DROP_RATE          5     // Out of 100, % chance of dropping
 
 int main(int argc, char **argv)
 {
@@ -58,44 +58,53 @@ int main(int argc, char **argv)
             continue;
       }
 
-
-      printf("START Sending datagram...\n");
-      printf("   CLIENT => client_len=%d, n=%d\n", client_len, n);
-
-      // Modify the buffer to simulate dropped packets
-      int m = 0;	// Size of modified buffer
-      int i, j, r;	// i: index of original buffer
-      				// j: index of new buffer
-      				// r: random number deciding whether to drop packet
-      char droppedBuf[n];
-      printf("   START iterating over packets! BufIn.len = %d\n", n);
-      printf("      ");
-      for(i=0, j=0; i < n; i++) {
-      	r = 1 + (rand() % 100);	// range of 1-100
-         printf("(%2d)", i);
-      	if( r > DROP_RATE ) {
-      	// if( i%2 == 0 ) {
-            printf(".");
-      		// Don't drop the packet
-      		droppedBuf[j] = buf[i];
-      		j++;
-      	} else {
-            printf("DROP\n      ");
-      		// By not copying over the element, we "drop" it
-      		// DO NOTHING, intentionally
-      	}
-      }
-      printf("\n   END iterating over packets! BufOut.len = %d\n", j);
-
-      if (sendto(sd, droppedBuf, j, 0, 
-      (struct sockaddr *)&client, client_len) != n) {
-            fprintf(stderr, "END [FAILURE] Can't send datagram\n");
-            continue;
-      } else {
-         printf("END [SUCCESS] Datagram sent\n");
-      }
+      sendBasic(client_len, client, sd, buf, n);
    }
    close(sd);
    return(0);
 }
 
+/**
+ * The initial implementation, with no reliability.
+ * It randomly drops packets based on constant at top of file
+ **/
+int sendBasic(int client_len, struct sockaddr_in client, int sd, char *buf, int n)
+{
+
+   printf("START [BASIC] Sending datagram...\n");
+   printf("   CLIENT => client_len=%d, n=%d\n", client_len, n);
+
+   // Modify the buffer to simulate dropped packets
+   int m = 0;  // Size of modified buffer
+   int i, j, r;   // i: index of original buffer
+               // j: index of new buffer
+               // r: random number deciding whether to drop packet
+   char droppedBuf[n];
+   printf("   START iterating over packets! BufIn.len = %d\n", n);
+   printf("      ");
+   for(i=0, j=0; i < n; i++) {
+      r = 1 + (rand() % 100); // range of 1-100
+      printf("(%2d)", i);
+      if( r > DROP_RATE ) {
+      // if( i%2 == 0 ) {
+         printf(".");
+         // Don't drop the packet
+         droppedBuf[j] = buf[i];
+         j++;
+      } else {
+         printf("DROP\n      ");
+         // By not copying over the element, we "drop" it
+         // DO NOTHING, intentionally
+      }
+   }
+   printf("\n   END iterating over packets! BufOut.len = %d\n", j);
+
+   if (sendto(sd, droppedBuf, j, 0, 
+   (struct sockaddr *)&client, client_len) != n) {
+         fprintf(stderr, "END [FAILURE] Can't send datagram\n");
+         return 1;
+   } else {
+      printf("END [SUCCESS] Datagram sent\n");
+      return 0;
+   }
+}
