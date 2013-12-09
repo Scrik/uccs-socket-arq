6bb095abea9fd88b2cccf04e9c532bc450933174
Ross Nordstrom <nordstrom.ross@gmail.com>
2013-12-08 23:41:43 -0700

    Update script to run on two different frame sizes as well

--- udp.sh
+++ udp.sh
@@ -5,9 +5,9 @@ PROTOCOL_SR=3
 
 FILENAMES=("README.md" "PDDModel_Network99.pdf")
 OUT_DIR="out"
-DROP_RATES=(00 15)
+DROP_RATES=(05 25)
 PROTOCOLS=($PROTOCOL_SAW $PROTOCOL_GBN)
-FRAME_SIZE=4096
+FRAME_SIZES=(512 2048)
 DIFF_CODE_SUM=0
 
 echo "####################################################################################################"
@@ -15,7 +15,7 @@ echo "# BEGIN CLIENT/SERVER DEMO                                                
 echo "# FILENAMES     = ${FILENAMES[@]}                                         "
 echo "# DROP_RATES    = ${DROP_RATES[@]}                                        "
 echo "# PROTOCOLS     = ${PROTOCOLS[@]}                                         "
-echo "# FRAME_SIZE    = $FRAME_SIZE                                             "
+echo "# FRAME_SIZES   = ${FRAME_SIZES[@]}                                       "
 echo "###                                                                       "
 
 # Clear the pkg directory to avoid confusion
@@ -36,67 +36,73 @@ then
 fi
 
 # Iterate over the various protocols
-for protocol in "${PROTOCOLS[@]}"
+for frameSize in "${FRAME_SIZES[@]}"
 do
+echo "Frame Size: $frameSize"
 
-echo "Protocol: $protocol"
-
-	# Iterate over the various drop rates
-	for dropRate in "${DROP_RATES[@]}"
+	# Iterate over the various protocols
+	for protocol in "${PROTOCOLS[@]}"
 	do
-		serverLogName="$OUT_DIR/protocol-$protocol/loss-$dropRate.server.log"
 
-		echo "   Starting server in background and logging output to '$serverLogName'..."
-		# Start the server in bkd and get its PID
-		./server.o $dropRate $protocol > $serverLogName &
-		PID_S=$!
-		echo "   Server started with PID $PID_S"
+	echo "Protocol: $protocol"
 
-		# Iterate over the input files
-		for filename in "${FILENAMES[@]}"
+		# Iterate over the various drop rates
+		for dropRate in "${DROP_RATES[@]}"
 		do
-			clientLogName="$OUT_DIR/protocol-$protocol/loss-$dropRate.client.log"
-			outputFilename="$OUT_DIR/protocol-$protocol/loss-$dropRate.$filename"
+			serverLogName="$OUT_DIR/protocol-$protocol/size-$frameSize.loss-$dropRate.server.log"
 
-			echo "      Starting client in background and logging output to '$clientLogName'..."
-			echo "      Saving echo file to '$outputFilename'."
-			./client.o -s $FRAME_SIZE $(hostname) $filename $outputFilename $protocol > $clientLogName &
-			PID_C=$!
-			echo "      Client started with PID $PID_C"
+			echo "   Starting server in background and logging output to '$serverLogName'..."
+			# Start the server in bkd and get its PID
+			./server.o $dropRate $protocol > $serverLogName &
+			PID_S=$!
+			echo "   Server started with PID $PID_S"
 
-			echo "      Waiting for client to stop running @$PID_C..."
-			echo -n "      "
-			while kill -0 $PID_C > /dev/null 2>&1; do
-				echo -n "."
-				sleep 0.5
+			# Iterate over the input files
+			for filename in "${FILENAMES[@]}"
+			do
+				clientLogName="$OUT_DIR/protocol-$protocol/size-$frameSize.loss-$dropRate.client.log"
+				outputFilename="$OUT_DIR/protocol-$protocol/size-$frameSize.loss-$dropRate.$filename"
+
+				echo "      Starting client in background and logging output to '$clientLogName'..."
+				echo "      Saving echo file to '$outputFilename'."
+				./client.o -s $frameSize $(hostname) $filename $outputFilename $protocol > $clientLogName &
+				PID_C=$!
+				echo "      Client started with PID $PID_C"
+
+				echo "      Waiting for client to stop running @$PID_C..."
+				echo -n "      "
+				while kill -0 $PID_C > /dev/null 2>&1; do
+					echo -n "."
+					sleep 0.5
+				done
+				echo " "
+				echo "      Client done."
+
+				echo "      Dumping DIFF result to '$outputFilename.diff'..." 
+				diff "$filename" "$outputFilename" > "$outputFilename.diff"
+				DIFF_CODE=$?
+				DIFF_CODE_SUM=$((DIFF_CODE_SUM + $DIFF_CODE))
+				RESULT="FAILURE[         ]"
+				if [ $DIFF_CODE -eq 0 ]
+				then
+					RESULT="SUCCESS[  YAY!   ]"
+				elif [ $DIFF_CODE -eq 1 ]
+				then
+					RESULT="FAILURE[DIFFERENT]"
+				elif [ $DIFF_CODE -eq 2 ]
+				then
+					RESULT="FAILURE[ TROUBLE ]"
+				else
+					RESULT="FAILURE[ UNKNOWN ]"
+				fi
+				echo "   '-> $RESULT"
 			done
-			echo " "
-			echo "      Client done."
 
-			echo "      Dumping DIFF result to '$outputFilename.diff'..." 
-			diff "$filename" "$outputFilename" > "$outputFilename.diff"
-			DIFF_CODE=$?
-			DIFF_CODE_SUM=$((DIFF_CODE_SUM + $DIFF_CODE))
-			RESULT="FAILURE[         ]"
-			if [ $DIFF_CODE -eq 0 ]
-			then
-				RESULT="SUCCESS[  YAY!   ]"
-			elif [ $DIFF_CODE -eq 1 ]
-			then
-				RESULT="FAILURE[DIFFERENT]"
-			elif [ $DIFF_CODE -eq 2 ]
-			then
-				RESULT="FAILURE[ TROUBLE ]"
-			else
-				RESULT="FAILURE[ UNKNOWN ]"
-			fi
-			echo "   '-> $RESULT"
+			echo "   Forceably stop the server @$PID_S..."
+			kill $PID_S
+			echo "   Server done."
+
 		done
-
-		echo "   Forceably stop the server @$PID_S..."
-		kill $PID_S
-		echo "   Server done."
-
 	done
 done
 
